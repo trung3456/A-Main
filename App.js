5be25115cc933e3757852a860384979f1235a26f
Trung Trần <mikuyoshida707@gmail.com>
2022-08-20 08:54:17 +0700

    2

---
+++ App.js
@@ -0,0 +1,22 @@
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import Home from './screens/Home'
+
+// export default function App() {
+//   return <Home/>
+// }
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './screens/Home'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default Home;
